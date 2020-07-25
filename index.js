4815c92e90070e714252cf79e45b918977586f1a
Fernando Michea <fernandomichea@MacBook-Air-de-Fernando.local>
2020-07-25 16:52:28 -0400

    cambio del log port

--- index.js
+++ index.js
@@ -49,15 +49,17 @@ app.use(cors());
 app.use('/',route())
 
 const host =process.env.HOST || 'localhost'
-const port =process.env.PORT || 5001
+const port =process.env.PORT || 5000
 app.listen(port,host,()=>
 {
     //console.log(process.env.PORT);
     //console.log(port);
     
-    console.log(process.env.DB_URL);
-    console.log(process.env.FRONTEND_URL);
+    //console.log(process.env.DB_URL);
+    //console.log(process.env.FRONTEND_URL);
 
+    console.log("esta conectado");
+    
 
 
     
